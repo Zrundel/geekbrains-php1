d012b1a1437e46fd693da83251cd676827002ce1
Sergey Zubkov <zru.mail@gmail.com>
2022-09-25 17:11:33 +0000

    Homework 3

--- lesson3/task_1.php
+++ lesson3/task_1.php
@@ -3,8 +3,8 @@
     $array_length = readline("Введите размер массивов ");
     //Генерируем массивы 
     for ($i=0; $i < $array_length; $i++) { 
-       $array1[i] = random_int(0 , 100);
-       $array2[i] = random_int(0 , 100);
+       $array1[$i] = random_int(0 , 100);
+       $array2[$i] = random_int(0 , 100);
     }
 
     //Выведем массивы для наглядности
@@ -13,6 +13,8 @@
 
     // Поэлементно перемножаем массивы и записываем массив с результатами 
     for ($i=0; $i < $array_length; $i++) { 
-        $resultArray[i] = $array1[i] * $array2[i];
+        $resultArray[$i] = $array1[$i] * $array2[$i];
      }
+     //Результат умножения массивов 
+     echo "Результат умножения массивов: ".implode('| ', $resultArray)."\n";
 ?>
